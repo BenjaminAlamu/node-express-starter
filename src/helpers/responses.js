6287f54d06c1759c2c12acdbb78d8cf115dae6f6
Benjamin Alamu <oluwaseunalamu@gmail.com>
2020-10-25 11:17:12 +0100

    Updated responses

--- src/helpers/responses.js
+++ src/helpers/responses.js
@@ -1,23 +1,32 @@
-export const UNAUTHORIZED = (res, message) =>
-  res.status(401).json({ status: error, error_code: 401, message });
+export const UNAUTHORIZED = (
+  res,
+  message = "You are authorized to access this route",
+  data
+) => res.status(401).json({ status: error, error_code: 401, message, data });
 
-export const CREATED = (res, data) =>
-  res.status(201).json({ status: 201, data });
+export const CREATED = (res, message = "Resource successfully created", data) =>
+  res.status(201).json({ status: 201, message, data });
 
-export const SUCCESS = (res, data) =>
-  res.status(200).json({ status: 200, data });
+export const SUCCESS = (res, message = "Successful", data) =>
+  res.status(200).json({ status: 200, message, data });
 
-export const BAD_REQUEST = (res, message) =>
-  res.status(400).json({ status: "error", error_code: 400, message });
+export const BAD_REQUEST = (
+  res,
+  message = "An error occured, please the data you are sending",
+  data
+) => res.status(400).json({ status: "error", error_code: 400, message, data });
 
-export const NOT_FOUND = (res, message) =>
-  res.status(404).json({ status: "error", error_code: 404, message });
+export const NOT_FOUND = (res, message = "Resource not found", data) =>
+  res.status(404).json({ status: "error", error_code: 404, message, data });
 
-export const CONFLICT = (res, message) =>
-  res.status(409).json({ status: "error", error_code: 409, message });
+export const CONFLICT = (res, message = "Resource has conflicts", data) =>
+  res.status(409).json({ status: "error", error_code: 409, message, data });
 
-export const NO_CONTENT = (res, message) =>
-  res.status(204).json({ status: 204, message });
+export const NO_CONTENT = (res, message = "Operation Successful", data) =>
+  res.status(204).json({ status: 204, message, data });
 
-export const SERVER_ERROR = (res, message) =>
-  res.status(500).json({ status: "error", error_code: 500, message });
+export const SERVER_ERROR = (
+  res,
+  message = "An error occurred, its not you its from us",
+  data
+) => res.status(500).json({ status: "error", error_code: 500, message, data });
